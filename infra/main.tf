757e43aa2e5535d87601f02c67360d6ef43ce606
hxrsh3110 <harshbankar31@gmail.com>
2026-07-26 23:16:36 +0530

    Replace SSH ingress with SSM instance profile for hotspot-safe access

--- infra/main.tf
+++ infra/main.tf
@@ -7,13 +7,6 @@ resource "aws_security_group" "honeypot_sg" {
   description = "launch-wizard-7 created 2026-07-06T10:49:50.673Z"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["27.59.103.156/32"]
-  }
-
-  ingress {
     description = "honeypot-trap"
     from_port   = 2222
     to_port     = 2222
@@ -34,6 +27,7 @@ resource "aws_instance" "honeypot" {
   instance_type          = "t3.micro"
   key_name               = "kharghar-server-key"
   vpc_security_group_ids = [aws_security_group.honeypot_sg.id]
+  iam_instance_profile    = aws_iam_instance_profile.ssm_profile.name
 
   user_data                   = file("${path.module}/user_data.sh")
   user_data_replace_on_change = true
@@ -46,4 +40,29 @@ resource "aws_instance" "honeypot" {
 resource "aws_eip" "honeypot_eip" {
   instance = aws_instance.honeypot.id
   domain   = "vpc"
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "honeypot-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "honeypot-ssm-profile"
+  role = aws_iam_role.ssm_role.name
 }
